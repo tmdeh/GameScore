0ad18132f9301192c72d5dd6b6c556bda8be5380
tmdeh <66948129+tmdeh@users.noreply.github.com>
2021-12-19 11:51:17 +0900

    Initial commit

---
+++ src/test/java/com/example/gamescore/GameScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
