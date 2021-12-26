5c8a0db67132296e2c7f34cfdac55283fad2d0d9
tmdeh <66948129+tmdeh@users.noreply.github.com>
2021-12-26 13:01:32 +0900

    SecurityConfig

--- src/main/java/com/gamescore/config/SecurityConfig.java
+++ src/main/java/com/gamescore/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gamescore.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/member/**")
                 .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     }
 }
